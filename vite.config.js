5c8c065c1ea44a98eec97863a3ed12131a44db8a
letrude <angeangegrimaud@gmail.com>
2026-02-05 15:12:49 +0100

    fix: performance vite-plugin-compression + SEO and CV

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCompression()],
   base: "/",
 });
